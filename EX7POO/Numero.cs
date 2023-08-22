c107af660024fd279d49d14355748c4d6bd5a1db
LucasAraujo1953 <130591551+LucasAraujo1953@users.noreply.github.com>
2023-08-22 18:52:13 -0300

    Add files via upload

---
+++ EX7POO/Numero.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX7POO
+{
+    internal class Numero
+    {
+        public int ObterMaiorValor(int a, int b)
+        {
+            return Math.Max(a, b);
+        }
+    }
+}
---
+++ EX7POO/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX7POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro valor:");
+            int valor1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor:");
+            int valor2 = Convert.ToInt32(Console.ReadLine());
+
+            Numero calculadora = new Numero();
+            int maiorValor = calculadora.ObterMaiorValor(valor1, valor2);
+
+            Console.WriteLine("O maior valor é: " + maiorValor);
+        }
+    }
+}
